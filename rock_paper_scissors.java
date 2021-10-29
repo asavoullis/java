7dc98f2b93246ecac708593de1f0945cf4b5ee02
unknown <cs16538@bristol.ac.uk>
2021-10-29 18:28:49 +0300

    version 2 - catches errors - added quit

--- rock_paper_scissors.java
+++ rock_paper_scissors.java
@@ -6,62 +6,98 @@ import java.util.Scanner;
 class Main {
 	//Main method
 	public static void main(String[] args) {
-	  //create a new Scanner which i will use to get  the input from the User
+		//Create a new Scanner which i will use to get  the input from the User
 		Scanner in = new Scanner(System.in);
-	  
-    //initialize a count variable to count the score 
-    //positive = user is winning 
-    int count;
-    count = 0;
-
-	  //main game loop
+		
+    //main game loop
 		while(true) {
-			//print a question RPS?
-			System.out.print("Choose rps ");
-		
-			//Store what the user enters into this string variable called move
-			String move = in.nextLine();
-		  
-			//Make a variable called cMove where we will have the choice of the computer stored in - the choise is random
-			String cMove = Character.toString("rps".charAt ((int)(Math.random()*3)));
-		
-			//Print out the computers move
-			System.out.println("Computer chooses: " + cMove);
-	
-			//if user's move is the same as the computers
-			if(move.equals(cMove)) {
-        //Print It's a Tie
-				System.out.println("It's a Tie");
-        //prints the score
-        System.out.println("The score is " + count);
-        //print an empty line
-        System.out.println("");
-		
-			// WIN CONDITIONS by the user
-			//if the user's move is rock and the computer's move is scissors 
-			//OR if the user's move is sciscors and the computer's move is paper 
-			//OR if the user's move is paper and the computer's move is rock
-			}else if 
-        ((move.equals("r") && cMove.equals("s")) || (move.equals("s") && cMove.equals("p")) || (move.equals("r") && cMove.equals("s"))){
-        //incriment the score
-        count ++;
-        //print You Win!
-			  System.out.println("You Win!");
 
-        //Print the count and new line
-        System.out.println("The score is " + count);
-        System.out.println("");
-
-      } else {
-        //decrese the count 
-        count --;
-        //Print You Lose!
-        System.out.println("You Lose!");
-
-        //Print the score and new line
-        System.out.println("The score is " + count);
-        System.out.println("");
         
+      //Print instructions
+      System.out.println("Enter your move. Type in r for rock, p for paper or s for scissors. If you want to exit the game, type q: ");
+      
+      //print in the same line 
+      System.out.print("Your move: ");
+      //Initialize the User's move
+      String myMove = in.nextLine();
+      
+      //Check if the User wants to exit the game
+      if (myMove.equals("q")){
+        System.out.println("");
+        System.out.println("Thanks for Playing!");
+        break;
+      }
+
+      //initialize a count variable to count the score 
+      //positive = user is winning 
+      int count;
+      //se the count variable to zero
+      count = 0;
+      
+      // .equals for strings 
+      // == for ints
+      
+      //Verify that the User's move is valid
+      // if myMove doesn't equal rock/paper/scissors
+      if(!myMove.equals("r") && !myMove.equals("s") && !myMove.equals("p")) {
+        //print its invlaid
+        System.out.println("Your move isn't valid!");
+      }else {
+      
+        //Randomly generate the opponents move (0, 1, 2)
+        int rand = (int)(Math.random() * 3);
+        
+        //initialize the computer's move variable as opponentMove and set it to an empty String 
+        String opponentMove = "";
+        
+        //if the random number is equal to 0 then set the opponentMove varaible equal to rock
+        if (rand == 0) {
+          opponentMove = "r";
+        
+        ////if the random number is equal to 1 then set the opponentMove varaible equal to paper
+        } else if (rand == 1) {
+          opponentMove = "p";
+          
+        //ELSE - OR if the random number is equal to 2 then set the opponentMove varaible equal to scissors
+        } else {
+          opponentMove = "s";
+        }
+        //print the computer's move - using string concatination
+        System.out.println("Opponent move: " + opponentMove);
+        
+        
+        //Calculate if the user / lost / tied
+        
+        //if user's move is the same as the computers 
+        if( myMove.equals(opponentMove)){
+          System.out.println("You Tied!");
+
+          //Print the count and new line 
+          System.out.println("The score is " + count);
+        }
+        // WIN CONDITIONS by the user
+        //if the user's move is rock and the computer's move is scissors 
+        //OR if the user's move is scissors and the computer's move is paper 
+        //OR if the user's move is paper and the computer's move is rock
+        else if((myMove.equals("r") && opponentMove.equals("s")) || (myMove.equals("s") && opponentMove.equals("p")) || (myMove.equals("r") && opponentMove.equals("s"))){
+          //incriment the score
+          count ++;
+          //print You Win!
+          System.out.println("You Win!");
+
+          //Print the count and new line 
+          System.out.println("The score is " + count);
+          System.out.println("");
+        }else{//decrese the count 
+          count --;
+          //Print You Lose!
+          System.out.println("You Lose!");
+
+          //Print the score and new line
+          System.out.println("The score is " + count);
+          System.out.println("");
+        
+        }
       }
     }
   }
